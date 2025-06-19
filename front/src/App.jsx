e7a739008ea460aab727b65ccf4ca32e9b807320
jinsu009 <chlrkd_009@naver.com>
2025-06-19 17:05:12 +0900

    footer , copyright

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import Footer from "./Components/Footer/Footer.jsx";
 import Navbar from "./Components/Navbar/Navbar.jsx";
 
 import { BrowserRouter } from "react-router-dom";
@@ -7,6 +8,7 @@ function App() {
   return (
     <BrowserRouter>
       <Navbar />
+      <Footer />
     </BrowserRouter>
   );
 }
