f5b0bf199cfe8b96a5b17a7ef8939753817438f1
jinsu009 <chlrkd_009@naver.com>
2025-06-22 14:45:24 +0900

    connect Mongoose

--- back/index.js
+++ back/index.js
@@ -1,10 +1,22 @@
+require("dotenv").config();
+
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log(" 성공 ");
+  })
+  .catch((error) => {
+    console.log(" 실패 ", error);
+  });
 
 app.listen(PORT, () => {
   console.log("Server is running");
